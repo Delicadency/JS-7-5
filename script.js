5eba353688e6b4bcbcd737ef8662b726c1874729
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-25 18:52:30 +0200

    deleted comment

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* Stwórz funkcję createButton tworzącą element <button> o identyfikatorze (id) "super-button", dowolnej nazwie (tekście) w środku i dodaj go do elementu <body>. Funkcja powinna również zwracać (return) tego buttona. Następnie stwórz funkcję openAlert, która będzie uruchamiała wbudowaną funkcję window.alert z tekstem "super". Funkcja openAlert powinna zostać wywołana w momencie kliknięcia na utworzony przycisk "super-button". */
-
 const createButton = () => {
   const button = document.createElement("button");
   button.id = "super-button";
